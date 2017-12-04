c5d12dc1633a36d23f5b2d898be1d78fd3da3938
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2017-12-04 17:04:08 +0800

    base header and footer

--- tRic/urls.py
+++ tRic/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name = 'index'),
-    # url(r'^contact/$', views.contact, name='contact'),
-    # url(r'^methods/$', views.methods, name='methods'),
-    # url(r'^download/$', views.download, name='download')
+    url(r'^$', views.index, name='index'),
+    url(r'^contact/$', views.contact, name='contact'),
+    url(r'^methods/$', views.methods, name='methods'),
+    url(r'^statistics/$', views.statistics, name='statistics'),
 ]
--- tRic/views.py
+++ tRic/views.py
@@ -4,7 +4,25 @@ from django.shortcuts import render
 
 # main home page
 def index(request):
-    title = "tric"
+    title = "tRic"
     context = {"title": title}
 
     return render(request, "tric/index.html", context, status=200)
+
+def contact(request):
+    title = "Contact"
+    context = {"title": title}
+
+    return render(request=request, template_name="tric/contact.html", context=context, status=200)
+
+def methods(request):
+    title = "Methods"
+    context = {"title": title}
+
+    return render(request=request, template_name="tric/methods.html", context=context, status=200)
+
+def statistics(request):
+    title = "Statistics"
+    context = {"title": title}
+
+    return render(request=request, template_name="tric/statistics.html", context=context, status=200)
